15f42fa5824fe80152b89debab5b96afc690d286
rupesh <kingrupesh5555@gmail.com>
2024-11-22 11:13:34 +0530

    initial commit

---
+++ data.js
@@ -0,0 +1,14 @@
+// data.js
+const userData = {
+    name: "Rupesh Ray",
+    bank: "BOB Bank - 2309",
+    amount: 1000,
+    refNo: "XX 0822",
+    date: "19 Oct, 06:23 PM",
+    prepaidNumber: "8882522323",
+    prepaidStatus: "₹349 plan expired 1 day(s) ago"
+};
+
+function getData() {
+    return userData;
+}
